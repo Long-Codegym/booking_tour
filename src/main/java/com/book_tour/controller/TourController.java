235b93f2830d26e93709952824a9480ef0cc514e
TungSmile <tungmonkey1101@gmail.com>
2023-10-24 09:44:53 +0700

    FT

--- src/main/java/com/book_tour/controller/TourController.java
+++ src/main/java/com/book_tour/controller/TourController.java
@@ -1,11 +1,29 @@
 package com.book_tour.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.book_tour.model.Tour;
+import com.book_tour.service.ITourService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/tours")
 public class TourController {
+    @Autowired
+    ITourService iTourService;
+
+    @GetMapping("/allTour")
+    public ResponseEntity<List<Tour>> getAllTour() {
+        return new ResponseEntity<>(iTourService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id_Tour}")
+    public ResponseEntity<Tour> getTourByID(@PathVariable long id_Tour) {
+        return new ResponseEntity<>(iTourService.getById(id_Tour), HttpStatus.OK);
+    }
+    @GetMapping
 }
--- src/main/java/com/book_tour/service/impl/TourServiceImpl.java
+++ src/main/java/com/book_tour/service/impl/TourServiceImpl.java
@@ -1,35 +1,39 @@
 package com.book_tour.service.impl;
 
 import com.book_tour.model.Tour;
+import com.book_tour.repository.ITourRepository;
 import com.book_tour.service.ITourService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TourServiceImpl implements ITourService {
+    @Autowired
+    ITourRepository iTourService;
     @Override
     public List<Tour> getAll() {
-        return null;
+        return iTourService.findAll();
     }
 
     @Override
     public Tour getById(long id) {
-        return null;
+        return iTourService.getReferenceById(id);
     }
 
     @Override
     public Tour create(Tour tour) {
-        return null;
+        return iTourService.save(tour);
     }
 
     @Override
     public Tour edit(Tour tour) {
-        return null;
+        return iTourService.save(tour);
     }
 
     @Override
     public void deleteById(long id) {
-
+        iTourService.delete(iTourService.getReferenceById(id));
     }
 }
